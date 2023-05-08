e95d3293b9672ce05993d808dbf434000ae8e4ad
dkatsuki <oniku.tabetai813@gmail.com>
2023-05-09 01:16:29 +0900

    typo

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -31,7 +31,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta property="og:title" content="Aigazo" />
           <meta property="og:type" content="website" />
         </Head>
