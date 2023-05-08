068dd4e0ed53ffdde30e24a7dc854e85cb2cae32
dkatsuki <oniku.tabetai813@gmail.com>
2023-05-09 00:42:03 +0900

    first commit

---
+++ src/pages/_app.jsx
@@ -0,0 +1,7 @@
+import '../globals.css'
+import React from 'react';
+
+export default function App({ Component, pageProps }) {
+  return(<Component {...pageProps} />);
+}
+
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+const SampleHome = () => {
+  return (
+    <div>
+      <h1>Sample Home</h1>
+    </div>
+  )
+}
+
+export default SampleHome
